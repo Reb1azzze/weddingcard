97f3de0917520ec07cbca2c0722445f6d367a2cc
Reb1azzze <82503613+Reb1azzze@users.noreply.github.com>
2025-04-04 17:27:37 +0300

    fix text

--- src/components/LocationSection/LocationSection.tsx
+++ src/components/LocationSection/LocationSection.tsx
@@ -5,7 +5,7 @@ const LocationSection = () => {
     return (
         <div className='location-section'>
             <span className='container-header'>Location</span>
-            <span className='container-text'>Орловский дворец</span>
+            <span className='container-text'>Ресторан Орловский дворец</span>
             <span className='container-text'>Адрес: Орловская ул., 209, посёлок Орловка, Набережные Челны</span>
         </div>
     );
